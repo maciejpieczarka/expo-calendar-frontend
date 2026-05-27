aa3a1fe37dfea07afca3054c5d56bbe0b8c1e38f
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2026-05-27 21:39:54 +0200

    feat: Add landing screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,82 +1,51 @@
-import { useAuth } from '@/context/authContext';
 import '@/global.css';
-import { AuthStore } from '@/lib/authStore';
+import { useAuthStore } from '@/lib/authStore';
 import { NAV_THEME } from '@/lib/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
 import { useEffect } from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-function RootLayoutContent() {
-  const { user, isLoading } = useAuth();
-
-  if (isLoading) {
-    return (
-      <View className="flex-1 justify-center items-center bg-white">
-        <ActivityIndicator size="large" color="#007AFF" />
-      </View>
-    );
-  }
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={user === null}>
-        <Stack.Screen name="index" />
-      </Stack.Protected>
-
-      <Stack.Protected guard={user === null}>
-        <Stack.Screen name="(auth)" />
-      </Stack.Protected>
-
-      <Stack.Protected guard={user !== null}>
-        <Stack.Screen name="(tabs)" />
-      </Stack.Protected>
-    </Stack>
-  );
-}
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
-  const isLoggedIn = AuthStore(state => state.isLoggedIn);
-  const isLoading = AuthStore(state => state.isLoading);
-  const checkAuth = AuthStore(state => state.checkAuth);
-  const hasCompletedOnboarding = AuthStore(
-    state => state.hasCompletedOnboarding
-  );
 
-  // Sprawdzenie czy token jwt istnieje juz na urzadzeniu
+  const { _hasHydrated, isLoggedIn, hasCompletedOnboarding } = useAuthStore();
+
   useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+    if (_hasHydrated) {
+      SplashScreen.hideAsync();
+    }
+  }, [_hasHydrated]);
 
-  if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
+  // NIE renderuj routingu dopóki zustand się nie zhydratuje
+  if (!_hasHydrated) {
+    return null;
   }
-
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(tabs)" />
-        </Stack.Protected>
+    <SafeAreaProvider>
+      <ThemeProvider value={NAV_THEME[colorScheme ?? 'dark']}>
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen name="(tabs)" />
+          </Stack.Protected>
 
-        <Stack.Protected guard={!isLoggedIn && !hasCompletedOnboarding}>
-          <Stack.Screen name="index" />
-        </Stack.Protected>
+          <Stack.Protected guard={!isLoggedIn && !hasCompletedOnboarding}>
+            <Stack.Screen name="index" />
+          </Stack.Protected>
 
-        <Stack.Protected guard={!isLoggedIn && hasCompletedOnboarding}>
-          <Stack.Screen name="(auth)" />
-        </Stack.Protected>
-      </Stack>
+          <Stack.Protected guard={!isLoggedIn && hasCompletedOnboarding}>
+            <Stack.Screen name="(auth)" />
+          </Stack.Protected>
+        </Stack>
 
-      <PortalHost />
-    </ThemeProvider>
+        <PortalHost />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
